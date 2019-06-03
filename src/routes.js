de5ad95082d6c1af639792e4076c29bc6cda1ed9
Gustakitos <luisgustavolg1033@gmail.com>
2019-06-03 13:50:50 -0300

    CRUD finished with Cors added

--- src/routes.js
+++ src/routes.js
@@ -3,7 +3,6 @@ const routes = express.Router()
 
 const ProductController = require('./controllers/ProductController')
 
-//Primeira rota
 routes.get('/products', ProductController.index)
 routes.get('/products/:id', ProductController.show)
 routes.post('/products', ProductController.store)
